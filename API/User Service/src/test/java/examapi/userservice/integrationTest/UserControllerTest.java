12b172d3c68e57efb1ad77dcab49acdabeb0f3b5
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-10-21 11:09:40 +0300

    Integration Tests. 92% line coverage

--- API/User Service/src/test/java/examapi/userservice/integrationTest/UserControllerTest.java
+++ API/User Service/src/test/java/examapi/userservice/integrationTest/UserControllerTest.java
@@ -1,7 +1,9 @@
 package examapi.userservice.integrationTest;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import examapi.userservice.domain.dto.UserEntityDto;
+import examapi.userservice.domain.entity.AllUsers;
 import examapi.userservice.innerSecurity.ApiKey;
 import examapi.userservice.repository.UserEntityRepository;
 
@@ -16,11 +18,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.util.Assert;
-import org.springframework.util.MultiValueMap;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,7 +39,9 @@ public class UserControllerTest {
     private ObjectMapper objectMapper;
 
     private static final String apiPass = "123456";
+
     private UserEntityDto user1;
+    private UserEntityDto user2;
 
     @BeforeEach
     public void setUp() {
@@ -51,6 +50,11 @@ public class UserControllerTest {
         this.user1 = new UserEntityDto();
         this.user1.setUsername("Vlado");
         this.user1.setPassword("123456");
+
+        this.user2 = new UserEntityDto();
+        this.user2.setUsername("Ignat");
+        this.user2.setPassword("123456");
+
 
         this.apiKey.setKey(apiPass);
     }
@@ -96,7 +100,115 @@ public class UserControllerTest {
 
        //assert
         Assertions.assertEquals(username, result1.getUsername());
+    }
 
+    @Test
+    public void should_Return_All_Users() throws Exception {
+        //arrange
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/register/" + apiPass)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(this.user1))
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/register/" + apiPass)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(this.user2))
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        //act
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/all/" + apiPass)
+        ).andReturn();
+
+        AllUsers result1 = this.objectMapper.readValue(result.getResponse().getContentAsString(),
+                AllUsers.class);
+
+        //assert
+        Assertions.assertEquals(2, result1.getAll().size());
+    }
+
+    @Test
+    public void should_Upgrade_To_Admin() throws Exception {
+        //arrange
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/register/" + apiPass)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(this.user1))
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/register/" + apiPass)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(this.user2))
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+
+        String username = this.user2.getUsername();
+        long id = this.userEntityRepository.findByUsername(username).orElse(null).getId();
+
+
+       //act
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .put("/update/" + id + "/" + apiPass)
+        );
+
+
+        MvcResult result =  this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/login/" + username + "/" + apiPass)
+        ).andReturn();
+
+        UserEntityDto result1 = this.objectMapper.readValue(result.getResponse().getContentAsString(),
+                UserEntityDto.class);
+
+        //assert
+        Assertions.assertEquals(2, result1.getRoles().size());
+    }
+
+    @Test
+    public void should_Downgrade_To_User() throws Exception {
+        //arrange
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/register/" + apiPass)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(this.user1))
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/register/" + apiPass)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(this.user2))
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        String username = this.user2.getUsername();
+        long id = this.userEntityRepository.findByUsername(username).orElse(null).getId();
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .put("/update/" + id + "/" + apiPass)
+        );
+
+
+        //act
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .put("/downgrade/" + id + "/" + apiPass)
+        );
+
+        MvcResult result =  this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/login/" + username + "/" + apiPass)
+        ).andReturn();
+
+        UserEntityDto result1 = this.objectMapper.readValue(result.getResponse().getContentAsString(),
+                UserEntityDto.class);
+
+        //assert
+        Assertions.assertEquals("USER", result1.getRoles().iterator().next().getRole());
     }
 
 }
